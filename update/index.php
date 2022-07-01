7039f425b9287c40dd34382d39763d3acadd5b4a
owsPro <elm@eadress.de>
2022-07-01 20:14:19 +0200

    Update index.php
    Fix:
    Copyright von HSE mit OpenWebSoccer-Sim korrigiert
    update/index.php

--- update/index.php
+++ update/index.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 /******************************************************
 
-HSE WebSoccer-Sim
+  This file is part of OpenWebSoccer-Sim.
 
-Copyright (c) 2013-2014 by
+  OpenWebSoccer-Sim is free software: you can redistribute it
+  and/or modify it under the terms of the
+  GNU Lesser General Public License
+  as published by the Free Software Foundation, either version 3 of
+  the License, or any later version.
 
-Hofmann Software Engineering
-EMail: info@websoccer-sim.com
-Homepage: http://www.websoccer-sim.com
+  OpenWebSoccer-Sim is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
 
-THIS IS NOT FREEWARE! YOU NEED TO OBTAIN A
-VALID LICENCE IN ORDER TO BE ALLOWED TO USE
-THIS SOURCE CODE!
-
-DIES IST KEINE FREEWARE (KEINE KOSTENLOSE SOFTWARE).
-SIE MUESSEN EINE KORREKTE LIZENZ BESITZEN, UM DIESEN
-PROGRAMMCODE BENUTZEN ZU DUERFEN!
+  You should have received a copy of the GNU Lesser General Public
+  License along with OpenWebSoccer-Sim.
+  If not, see <http://www.gnu.org/licenses/>.
 
 ******************************************************/
 
@@ -44,9 +45,9 @@ include(BASE_FOLDER . "/classes/DbConnection.class.php");
  */
 function printWelcomeScreen() {
 	global $supportedLanguages;
-	
+
 	echo "<h2>Sprache wählen / Choose language</h2>";
-	
+
 	echo "<form method=\"post\">";
 	$first = TRUE;
 	foreach ($supportedLanguages as $langId => $langLabel) {
@@ -59,7 +60,7 @@ function printWelcomeScreen() {
 		echo "> $langLabel";
 		echo "</label>";
 	}
-	
+
 	echo "<button type=\"submit\" class=\"btn\">Wählen / Choose</button>";
 	echo "<input type=\"hidden\" name=\"action\" value=\"actionSetLanguage\">";
 	echo "</form>";
@@ -71,14 +72,14 @@ function actionSetLanguage() {
 		$errors[] = "Please select a language.";
 		return "printWelcomeScreen";
 	}
-	
+
 	global $supportedLanguages;
 	$lang = $_POST["lang"];
 	if (key_exists($lang, $supportedLanguages)) {
 		$_SESSION["lang"] = $lang;
 		return "printSystemCheck";
 	}
-	
+
 	return "printWelcomeScreen";
 }
 
@@ -87,17 +88,17 @@ function actionSetLanguage() {
  */
 function printSystemCheck($messages) {
 	echo "<h2>". $messages["check_title"] . "</h2>";
-	
+
 	?>
-	
+
 	<?php
-	
+
 	$requirments = array();
-	
+
 	$writableFiles = explode(",", WRITABLE_FOLDERS);
 	foreach ($writableFiles as $writableFile) {
 		$file = BASE_FOLDER . "/" . $writableFile;
-		
+
 		$requirments[] = array(
 				"requirement" => $messages["check_req_writable"] . " <i>" . $writableFile . "</i>",
 				"min" => $messages["check_req_yes"],
@@ -105,9 +106,9 @@ function printSystemCheck($messages) {
 				"status" => (is__writable($file)) ? "success" : "error"
 		);
 	}
-	
+
 	?>
-	
+
 	<table class="table">
 		<thead>
 			<tr>
@@ -117,7 +118,7 @@ function printSystemCheck($messages) {
 			</tr>
 		</thead>
 		<tbody>
-		<?php 
+		<?php
 		$valid = TRUE;
 		foreach($requirments as $requirement) {
 			echo "<tr class=\"".  $requirement["status"] . "\">";
@@ -125,7 +126,7 @@ function printSystemCheck($messages) {
 			echo "<td>" . $requirement["min"] . "</td>";
 			echo "<td>" . $requirement["actual"] . "</td>";
 			echo "</tr>";
-			
+
 			if ($requirement["status"] == "error") {
 				$valid = FALSE;
 			}
@@ -133,9 +134,9 @@ function printSystemCheck($messages) {
 		?>
 		</tbody>
 	</table>
-	
-	<?php 
-	
+
+	<?php
+
 	if ($valid) {
 		echo "<form method=\"post\">";
 		echo "<button type=\"submit\" class=\"btn\">". $messages["button_next"] . "</button>";
@@ -152,7 +153,7 @@ function actionMoveFiles() {
 	$fileNames = array("config.inc.php", "adminlog.php", "imprint.php", "entitylog.php");
 	$oldDir = BASE_FOLDER . "/admin/config/";
 	$newDir = BASE_FOLDER . "/generated/";
-	
+
 	foreach ($fileNames as $fileName) {
 		if (file_exists($oldDir . $fileName)) {
 			rename($oldDir . $fileName, $newDir . $fileName);
@@ -169,13 +170,13 @@ function actionMoveFiles() {
 function printFinalPage($messages) {
 	include(CONFIGFILE);
 	?>
-	
+
 	<div class="alert alert-success"><strong><?php echo $messages["final_success_alert"]; ?></strong></div>
-	
+
 	<div class="alert"><strong><?php echo $messages["final_success_note"]; ?></strong></div>
-	
+
 	<p><i class="icon-arrow-right"></i> <a href="<?php echo $conf["context_root"]; ?>/admin"><?php echo $messages["final_link"]; ?></a></p>
-	<?php 
+	<?php
 }
 ?>
 <!DOCTYPE html>
@@ -193,61 +194,61 @@ function printFinalPage($messages) {
     </style>
   </head>
   <body>
-  
+
 	<div class="container">
-	
+
 		<h1>Open WebSoccer-Sim Update Installation</h1>
-		
+
 		<hr>
-		
-		<?php 
-		
+
+		<?php
+
 		$errors = array();
-		
+
 		$messagesIncluded = FALSE;
 		if(isset($_SESSION["lang"])) {
 			include("messages_" . $_SESSION["lang"] . ".inc.php");
 			$messagesIncluded = $_SESSION["lang"];
 		}
-		
+
 		$action = (isset($_REQUEST["action"])) ? $_REQUEST["action"] : "";
 		if (!strlen($action) || substr($action, 0, 6) !== "action") {
 			$view = "printWelcomeScreen";
 		} else {
 			$view = $action();
 		}
-		
+
 		if(isset($_SESSION["lang"]) && $_SESSION["lang"] !== $messagesIncluded) {
 			include("messages_" . $_SESSION["lang"] . ".inc.php");
 		}
-		
+
 		if (count($errors)) {
 			foreach($errors as $error) {
 				echo "<div class=\"alert alert-error\">$error</div>";
 			}
 		}
-		
+
 		if (isset($messages)) {
 			$view($messages);
 		} else {
 			$view();
 		}
-		
+
 		?>
-	  
+
       <hr>
 
       <footer>
         <p>Powered by <a href="http://www.websoccer-sim.com" target="_blank">Open WebSoccer-Sim</a></p>
-      </footer>		  
+      </footer>
 	</div>
-	
+
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="../admin/bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
 
-<?php 
+<?php
 // real is_writable (http://www.php.net/manual/en/function.is-writable.php#73596)
 function is__writable($path) {
 	//will work in despite of Windows ACLs bug
